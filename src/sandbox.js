b3fcc2f58c5d5a6a77ff7f811fa89b78c87ab8ec
pad19 <pad19051999@gmail.com>
2020-12-21 10:42:04 +0200

    Add myReduce

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,9 +1,14 @@
-String.prototype.toStartCase = function () {
-  let newStr = this[0].toUpperCase();
-  for (let i = 1; i < this.length; i++) {
-    newStr += this[i];
+Array.prototype.myReduce = function (callback, initialValue = 0) {
+  let accumulator = initialValue;
+  for (let i = 0; i < this.length; i++) {
+    accumulator += this[i];
+    callback(accumulator, this[i], i, this);
   }
-  return newStr;
+  return accumulator;
 };
 
-console.log('hello world'.toStartCase());
+const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 56, 6];
+const reduceResult = array.reduce((acc, item) => acc + item, 0);
+console.log(reduceResult);
+const myReduceResult = array.myReduce((acc, item) => acc + item, 0);
+console.log(myReduceResult);
