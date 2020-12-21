97189220e9a2aba677135dcaebd0167344ca383f
pad19 <pad19051999@gmail.com>
2020-12-21 10:52:55 +0200

    Add myForEach

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,13 +1,11 @@
-Array.prototype.myMap = function (callback) {
-  const newArray = [];
+Array.prototype.myForEach = function (callback) {
   for (let i = 0; i < this.length; i++) {
-    newArray.push(callback(this[i], i, this));
+    callback(this[i], i, this);
   }
-  return newArray;
 };
 
 const array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-const mapResult = array.map((item, i, arr) => arr[i] * item);
+const mapResult = array.forEach((item, i, arr) => console.log(arr[i] * item));
 console.log(mapResult);
-const myMapResult = array.myMap((item, i, arr) => arr[i] * item);
+const myMapResult = array.myForEach((item, i, arr) => console.log(arr[i] * item));
 console.log(myMapResult);
