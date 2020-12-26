8ae65a30fefd1e91e25ace8007ab920d5593816c
pad19 <pad19051999@gmail.com>
2020-12-26 11:24:08 +0200

    Add Counter useMemo with object

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,5 +1,4 @@
-import React, {useContext, useEffect, useLayoutEffect, useMemo, useRef, useState} from 'react';
-import {MyContext} from '../App';
+import React, {useEffect, useMemo, useState} from 'react';
 import './styles.css';
 
 const someUselessFunc = a => {
@@ -11,63 +10,44 @@ const someUselessFunc = a => {
 };
 
 const Counter = () => {
-  const {red, green} = useContext(MyContext);
   const [count, setCount] = useState(0);
-  const [step, setStep] = useState(1);
-  const [textInputValue, setTextInputValue] = useState('');
-  const [prevState, setPrevState] = useState(null);
-  const ref = useRef(null);
-  const prevCountRef = useRef(null);
+  const [colored, setColored] = useState();
+  const styles = useMemo(() => ({
+    color: colored ? 'red' : 'green',
+  }), [colored]);
 
   const increment = () => {
+    setColored(false);
     setCount(prevState => {
-      setPrevState(prevState);
-      return prevState + step;
+      return prevState + 1;
     });
   };
 
   const decrement = () => {
+    setColored(true);
     setCount(prevState => {
-      setPrevState(prevState);
-      return prevState - step;
+      return prevState - 1;
     });
   };
 
   const cancel = () => {
     setCount(0);
-    setStep(1);
   };
-
-  const textInputHandler = event => {
-    setTextInputValue(event.target.value);
-  };
-
-  useLayoutEffect(() => {
-    prevCountRef.current = count;
-  });
 
   useEffect(() => {
-    prevCountRef.current = count;
-  });
-
-  const someUselessValue = useMemo(() => someUselessFunc(count), [count]);
+    console.log('Color changed');
+  }, [styles]);
 
   return (
     <div>
       <div>
-        <h1>Count: {count}</h1>
-        <h1>Prev count: {prevCountRef.current}</h1>
-        <h1>Prev state: {prevState}</h1>
-        <h1>Some useless value: {someUselessValue}</h1>
+        <h1 style={styles}>Count: {count}</h1>
       </div>
       <div>
-        <input type="number" value={step} onChange={event => setStep(Number(event.target.value))}/>
-        <br/>
-        <input ref={ref} type="text" value={textInputValue} onChange={textInputHandler}/>
       </div>
       <div>
-        <button className={green} onClick={increment}>+</button>
-        <button className={red} onClick={decrement}>-</button>
+        <button onClick={increment}>+</button>
+        <button onClick={decrement}>-</button>
         <button onClick={cancel}>Cancel</button>
       </div>
     </div>
