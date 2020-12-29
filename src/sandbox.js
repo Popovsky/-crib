2d546a144315736bb060c16630b592f1b9d8167d
pad19 <pad19051999@gmail.com>
2020-12-29 13:41:34 +0200

    Add Bubble sort

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,13 +1,16 @@
-const carriedSum = (...args) => {
-  let result = [].concat(args);
-  return function inner(...innerArgs) {
-    if (innerArgs.length) {
-      result = result.concat(innerArgs);
-      return inner;
+let array = [...new Array(10)].map(() => Math.round(Math.random() * 10));
+console.log(array);
+const bubbleSort = arr => {
+  let temp;
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] > arr[j]) {
+        temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+      }
     }
-    return result.reduce((acc, item) => acc + item, 0);
-  };
+  }
 };
-
-
-console.log(carriedSum(1)(2)(3)(4)(5)(6)());
+bubbleSort(array);
+console.log(array);
