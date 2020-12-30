960136ebf6a2167a8641efda725f8a2b8acd9f14
pad19 <pad19051999@gmail.com>
2020-12-30 13:29:39 +0200

    Add debounce

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,25 +1,15 @@
-let array = [...new Array(10)].map(() => Math.round(Math.random() * 10));
-console.log(array);
-const randomQuickSort = arr => {
-  if (arr.length < 2) {
-    return arr;
-  } else {
-    let middle = arr[Math.floor(Math.random() * arr.length)];
-    let less = [];
-    let equal = [];
-    let more = [];
-    for (let i = 0; i < arr.length; i++) {
-      if (arr[i] < middle) {
-        less.push(arr[i]);
-      }
-      if (arr[i] > middle) {
-        more.push(arr[i]);
-      }
-      if (arr[i] === middle) {
-        equal.push(arr[i]);
-      }
-    }
-    return [...randomQuickSort(less), ...equal, ...randomQuickSort(more)];
-  }
+const input = document.getElementById('input');
+const log = () => {
+  console.log(input.value);
+}
+const debounce = () => {
+  let timeout;
+  return function () {
+    clearTimeout(timeout);
+    timeout = setTimeout(log, 500);
+  };
 };
-console.log(randomQuickSort(array));
+
+const onChange = debounce();
+
+input.addEventListener('keyup', onChange);
